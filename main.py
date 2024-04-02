c1281898e7c59e11841cd76d6aa1ba55a66ca626
Kyle Matthews <linescrawler@gmail.com>
2024-04-02 19:33:34 +0100

    Updates to latest version of function, I have inspected the text file to find that the words and integers are scrambled, so the next update will include a function to order the num,word values.

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-
-
 def decode(message_file):
+    #Opens text file specified by user.
     with open(message_file, 'r') as file: 
         lines = file.readlines()
-    pyramid = []
     
+    pyramid = []
+    for line in lines:
+        num, word = line.strip().split(' ')
+        pyramid.append(word)
+
+    decoded_message = ''
+    index = 0
+    for i in range(1, len(lines)):
+        print(f"Index: {index}, Word: {pyramid[index]}")
+        decoded_message += pyramid[index] + ' '
+        index += i
+        if index >= len(pyramid):
+            break
+    return decoded_message.strip()
+
+#Example:
+decoded_message = decode('coding_qual_input.txt')
+print(decoded_message)
